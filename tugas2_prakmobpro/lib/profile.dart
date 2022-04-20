4fa038f57061fd36d28e35df84111d3a8fc867ba
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-04-20 10:11:04 +0700

    beres

--- tugas2_prakmobpro/lib/profile.dart
+++ tugas2_prakmobpro/lib/profile.dart
@@ -24,7 +24,7 @@ class _profilePageState extends State<profilePage> {
             children: [
               Image.asset(
                 'assets/mas.png',
-                height: 130,
+                height: 120,
               ),
               SizedBox(height: 15,),
               Text(
@@ -34,6 +34,100 @@ class _profilePageState extends State<profilePage> {
                   fontSize: 17
                 ),
               )
+            ],
+          ),
+          SizedBox(height: 50,),
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Text(
+              'Akun',
+              style: tiga.copyWith(
+                fontSize: 15,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 10),
+            child: Column(
+              children: [
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.fact_check),
+                    SizedBox(width: 10,),
+                    Text('Pesanan'),
+                    SizedBox(width: 75,),
+                    Text('Cek riwayat & pesanan aktif'),
+                    SizedBox(width: 10,),
+                    Icon(Icons.arrow_forward_ios, size: 18,)
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.notifications),
+                    SizedBox(width: 10,),
+                    Text('Notifikasi'),
+                    SizedBox(width: 75,),
+                    //Text('Cek riwayat & pesanan aktif'),
+                    SizedBox(width: 176,),
+                    Icon(Icons.arrow_forward_ios, size: 18,)
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.help_center),
+                    SizedBox(width: 10,),
+                    Text('Bantuan & Laporan Saya'),
+                    SizedBox(width: 75,),
+                    //Text('Cek riwayat & pesanan aktif'),
+                    SizedBox(width: 83,),
+                    Icon(Icons.arrow_forward_ios, size: 18,)
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.discount),
+                    SizedBox(width: 10,),
+                    Text('Voucher Saya'),
+                    SizedBox(width: 75,),
+                    //Text('Cek riwayat & pesanan aktif'),
+                    SizedBox(width: 150,),
+                    Icon(Icons.arrow_forward_ios, size: 18,)
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.security_sharp),
+                    SizedBox(width: 10,),
+                    Text('Kebijakan Privasi'),
+                    SizedBox(width: 75,),
+                    //Text('Cek riwayat & pesanan aktif'),
+                    SizedBox(width: 128,),
+                    Icon(Icons.arrow_forward_ios, size: 18,)
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 100,),
+          Column(
+            children: [
+              Text(
+                '@JASA BERKAH',
+                style: tiga.copyWith(
+                  color: ijo,
+                ),
+              ),
             ],
           )
         ],
